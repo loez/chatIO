44f597dd5889b75bbb39a1d90bffe5b29fb4a80c
Raphael Mesquita <raphael.heilbuth@gmail.com>
2022-01-26 13:54:05 -0300

    feat: Adicionado hora e retorno da msg enviada. #4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const moment = require('moment');
 const server = http.createServer(app);
 const io = require('socket.io')(server,{
     cors: {
@@ -22,18 +23,33 @@ server.listen(80, function () {
 io.on('connection', (socket) => {
     socket.on("login", ({nome, sala}) => {
         const { usuario } = adicionaUsuario(socket.id, nome, sala)
-        socket.join(usuario.sala)
-        socket.broadcast.emit('mensagem',usuario.nome + ' entrou');
+        socket.join(usuario.sala);
+        let mensagem = {
+            'Mensagem': 'Entrou',
+            'Hora': moment().format('HH:mm:ss'),
+            'Usuario': usuario.nome
+        };
+        socket.broadcast.to(usuario.sala).emit('mensagem', mensagem);
     });
 
-    socket.on("enviaMensagem", message => {
-        const usuario = getUsuario(socket.id)
-        socket.broadcast.to(usuario.sala).emit('mensagem',message);
+    socket.on("enviaMensagem", (message, callback) => {
+        const usuario = getUsuario(socket.id);
+
+        message['Hora'] = moment().format('HH:mm:ss');
+
+        socket.broadcast.to(usuario.sala).emit('mensagem', message);
+
+        callback(message);
     });
 
     socket.on("getSalas",() =>{
         const salas = getSalas();
         socket.emit('retornoSalas',salas);
+    });
+
+    socket.on("getUsers", (sala) =>{
+        const users = getUsuariosSala(sala);
+        socket.emit('retornoUsers', users);
     });
 
     socket.on("disconnect", () => {
